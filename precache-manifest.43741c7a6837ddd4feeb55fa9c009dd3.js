3f7d6e0d6b47e84da0e87c2025711496cf30f6ba
Jonathan Yin <jonathanyin.12@gmail.com>
2022-01-10 17:40:13 -0500

    Updates

---
+++ precache-manifest.43741c7a6837ddd4feeb55fa9c009dd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e1a8d7f89c5025d09e2",
+    "url": "/static/css/main.c6461338.chunk.css"
+  },
+  {
+    "revision": "8e1a8d7f89c5025d09e2",
+    "url": "/static/js/main.8e1a8d7f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "390f734cd1aaec2ba51f",
+    "url": "/static/js/2.390f734c.chunk.js"
+  },
+  {
+    "revision": "d8c648d34ad2e08472db9b5f3e2c0a8d",
+    "url": "/index.html"
+  }
+];
