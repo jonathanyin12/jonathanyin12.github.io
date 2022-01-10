97f984c6e36ac5941ffcb6ba0f0aa9aba15e81b0
Jonathan Yin <jonathanyin.12@gmail.com>
2022-01-10 17:49:49 -0500

    clean up particles, separate into json file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import './App.css';
-import ParticlesContainer from "./components/ParticlesContainer.jsx";
+import React from "react";
+import "./App.css";
 import About from "./components/About.jsx";
+import Particles from "react-tsparticles";
+import particlesOptions from "./particles.json";
 
 function App() {
-
-    return (
-        <div className='container'>
-          <About/>
-          <ParticlesContainer/>  
-        </div>
-      );
+  return (
+    <div className="container">
+      <About />
+      <Particles options={particlesOptions} />
+    </div>
+  );
 }
-   
+
 export default App;
